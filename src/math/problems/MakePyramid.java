2a130a71eac46436b9307224244f0d382cc832ff
sohag2018 <nafasatzayan@gmail.com>
2018-12-23 12:22:22 -0500

    2nd

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -3,16 +3,16 @@ package math.problems;
 public class MakePyramid {
     public static void main(String[] args) {
 
-        /*   Implement a large Pyramid of stars in the screen with java.
+        for(int i=0;i<50;i++) {  // took 50 numbers for the size of the pyramid
+            for(int j=0;j<50-i;j++) {
+                System.out.print(" ");
+            }
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
-
-        */
+            for (int k=0;k<=i;k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
 
         }
