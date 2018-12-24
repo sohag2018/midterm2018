d8cf89e2d85bd53426938dfdf067ef08e8082339
sohag2018 <nafasatzayan@gmail.com>
2018-12-24 14:29:43 -0500

    Ans of the Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -1,16 +1,23 @@
 package string.problems;
 
-/**
- * Created by mrahman on 04/22/17.
- */
 public class Permutation {
 
     public static void main(String[] args) {
+        /*Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
+         * Write Java program to compute all Permutation of a String*/
+        permutation("ABC");
+    }
+    public static void permutation(String input) {
+        permutation(" ", input);
 
-        /*
-         * Permutation of String "ABC" is "ABC" "BAC" "BCA" "ACB" "CAB" "CBA".
-         * Write Java program to compute all Permutation of a String
-         *
-         */
+    }
+    public static void permutation(String perm, String word) {
+        if(word.isEmpty()) {
+            System.out.println(perm + word);
+        } else {
+            for (int i = 0; i<word.length(); i++) {
+                permutation(perm + word.charAt(i), word.substring(0,i) + word.substring(i + 1, word.length()));
+            }
+        }
     }
 }
