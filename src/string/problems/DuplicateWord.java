b1ff93b3cd06770466e3a793e931b6f6d6c23b32
sohag2018 <nafasatzayan@gmail.com>
2018-12-24 21:56:01 -0500

    CollectionView done

--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,10 @@
 package string.problems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -11,8 +16,36 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
 
-        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
-
+        String st = "Java is a programing language. PNT is the right place to learn Java ";
+        Set<String> duplicates = duplicateWords(st);
+        System.out.println("input : " + st);
+        System.out.println("output : " + duplicates);
     }
+    public static Set<String> duplicateWords(String input){
+        if(input == null || input.isEmpty()){
+            return Collections.emptySet();
+        }
+        Set<String> duplicates = new HashSet<>();
+        String[] words = input.split(" ");
+        double length = (double) input.length()/ words.length;
+        System.out.println("Average length of words: "+ length);
+        Set<String> set = new HashSet<>();
+        ArrayList<String> arraylist = new ArrayList<String>();
 
+        for(String word : words){
+            arraylist.add(word);
+            if(!set.add(word)){
+                duplicates.add(word);
+            }
+        }
+        for(String count : arraylist) {
+            if(Collections.frequency(arraylist, count)>1) {
+                System.out.println("Number of occurance of: "+count+" is: " + Collections.frequency(arraylist, count));
+            }
+        }
+        return duplicates;
+    }
 }
+
+
+
