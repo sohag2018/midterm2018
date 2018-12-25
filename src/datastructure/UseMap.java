73605dd9625a63507f8172bf0d32add91df2f85c
sohag2018 <nafasatzayan@gmail.com>
2018-12-24 22:56:33 -0500

    Ans of UseMap prob

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,7 +12,35 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+		List<String> Flowers = new ArrayList<>();
+		Flowers.add("Rose");
+		Flowers.add("Tulip");
 
+		List<String> Fish = new ArrayList<>();
+		Fish.add("Hilsha");
+		Fish.add("ButterFish");
+
+		List<String> Birds = new ArrayList<>();
+		Birds.add("Moyna");
+		Birds.add("Crow");
+
+		Map<String, List<String>> list = new HashMap<String, List<String>>();
+		list.put("Comon Flowers of Bd",Flowers);
+		list.put("Comon Fishes of Bd", Fish);
+		list.put("Comon Birds of Bd",Birds);
+		System.out.println(list);
+		System.out.println(" ");
+		System.out.println("Getting values by using for each loop.....");
+		for (Map.Entry<String, List<String>> st : list.entrySet()){
+			System.out.println(st.getKey()+ " --------> " +st.getValue());
+		}
+		System.out.println(" ");
+		System.out.println("Getting values by  using iterator.....");
+		Set set = list.entrySet();
+		Iterator it = set.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 	}
 
 }
